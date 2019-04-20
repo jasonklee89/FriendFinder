9cb948281c059ad42a2807c8942293b1e56e4a16
jason <jasonkellogglee@gmail.com>
2019-04-19 17:44:19 -0700

    finished setting up routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,21 +1,31 @@
-// Dependencies
-// =============================================================
-var express = require("express");
-var path = require("path");
+var friends = require("../data/friends");
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = 3000;
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
+    app.post("/api/friends", function (req, res) {
+    });
+}
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-// Basic route that sends the user first to the AJAX Page
 
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-});
+// var express = require("express");
+// var path = require("path");
+
+// // Sets up the Express App
+// // =============================================================
+// var app = express();
+// var PORT = 3000;
+
+// // Sets up the Express app to handle data parsing
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // Basic route that sends the user first to the AJAX Page
+
+// // Starts the server to begin listening
+// // =============================================================
+// app.listen(PORT, function () {
+//     console.log("App listening on PORT " + PORT);
+// });
--- app/server.js
+++ app/server.js
@@ -10,6 +10,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
