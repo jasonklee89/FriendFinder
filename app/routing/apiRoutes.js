1baa73219320acaa3d9aa1170da0f24458efc90a
jason <jasonkellogglee@gmail.com>
2019-04-19 19:22:56 -0700

    able to add survey results to friends array

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,6 +5,36 @@ module.exports = function (app) {
         res.json(friends);
     });
     app.post("/api/friends", function (req, res) {
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 100
+        };
+
+        console.log(req.body);
+
+        var userData = req.body;
+        var userScores = userData.scores;
+
+        // console.log(userScores);
+        
+        var totalDifference = 0;
+
+        for (var i = 0; i < friends.length; i++) {
+            console.log(friends[i]);
+            totalDifference = 0;
+
+            for(var j = 0; i < friends[i].scores[j]; j++) {
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+                if (totalDifference <= bestMatch.friendDifference) {
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.friendDifference = friends[i].friendDifference;
+                }
+            }
+        }
+        friends.push(userData);
+        res.json(bestMatch);
     });
 }
 
